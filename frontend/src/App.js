28301d6965b13d9652db1cdc04dbf509618eb39b
diky.nugroho <diky.nugroho@prosa.ai>
2022-11-21 23:33:52 +0700

    Add layout component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import Layout from "./component/layout";
+import { ThemeProvider } from "@mui/material";
+import theme from "./config/theme";
+import CssBaseline from "@mui/material/CssBaseline";
 
 const App = () => {
-	return <div>Photo album app</div>;
+	return (
+		<CssBaseline>
+			<ThemeProvider theme={theme}>
+				<Layout>Photo album app</Layout>
+			</ThemeProvider>
+		</CssBaseline>
+	);
 };
 
 export default App;
--- frontend/src/component/layout/header/Avatar.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Avatar = () => {
-	return <div>Avatar</div>;
-};
-
-export default Avatar;
