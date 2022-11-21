18056e3e2f605e582cbb7aff19e53fb4ef48d051
diky.nugroho <diky.nugroho@prosa.ai>
2022-11-21 22:45:01 +0700

    Add theme configuration

---
+++ frontend/src/config/theme.ts
@@ -0,0 +1,20 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: "#207780",
+			dark: "#16585F",
+		},
+		secondary: {
+			main: "#C8A15C",
+			light: "#FFE8D2",
+			dark: "#777167",
+		},
+	},
+	typography: {
+		fontFamily: ["Roboto", "sans-serif"].join(","),
+	},
+});
+
+export default theme;
