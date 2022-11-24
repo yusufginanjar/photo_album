88d1294f9c867761bb83e54904e4d7c7bbc909d0
yusufginanjar <yusufginanjar16@gmail.com>
2022-11-24 21:03:09 +0700

    FE: home page - Add button

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,14 +1,26 @@
 import * as React from "react";
 import Layout from "../component/layout";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import PhotoCamera from "@mui/icons-material/PhotoCamera";
+import Box from '@mui/material/Box';
 
-const Header = () => {
+
+
+const Home = () => {
     return (
         <>
             <Layout>
-				<h1>Hello</h1>
+                <Container maxWidth="xl" id="margin-one">
+                <Box mt={3} display="flex" justifyContent="center">
+                    <Button variant="contained" endIcon={<PhotoCamera />} size="large">
+                        Buat Album
+                    </Button>
+                </Box>
+                </Container>
 			</Layout>
         </>
     );
 };
 
-export default Header;
+export default Home;
